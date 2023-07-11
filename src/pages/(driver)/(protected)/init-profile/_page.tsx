181cf2b644ee0117c7c57da4174efcc014f26e12
vupham-salmon <vupham.productivity@gmail.com>
2023-07-11 20:10:27 +0700

    fix: remove `auth-decision` page

---
+++ src/pages/(driver)/(protected)/init-profile/_page.tsx
@@ -0,0 +1,12 @@
+import {Container, Typography} from "@mui/material"
+
+function InitProfilePage()
+{
+   return (
+      <Container>
+         <Typography>can't believe this shit is real, this is init profile page</Typography>
+      </Container>
+   )
+}
+
+export default InitProfilePage
