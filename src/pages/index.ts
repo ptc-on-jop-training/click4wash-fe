29d02857bc51e2aee70bac36c0ffb9ed08a18f56
vupham-salmon <vupham.productivity@gmail.com>
2023-07-11 20:11:53 +0700

    index

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 import RootLayout from "./root-layout.tsx"
-import DashboardPage from "./(driver)/(protected)/dashboard/_page.tsx"
+import HomePage from "./(driver)/(protected)/(home)/_page.tsx"
 import IntroductionPage from "./(driver)/introduction/_page.tsx"
 import DriverLayout from "./(driver)/_layout.tsx"
 import ProtectedDriverLayout from "./(driver)/(protected)/_layout.tsx"
@@ -7,14 +7,16 @@ import SystemLayout from "./system/_layout.tsx"
 import ProtectedSystemLayout from "./system/(protected)/_layout.tsx"
 import AdminPage from "./system/(protected)/admin/_page.tsx"
 import TeamMemberPage from "./system/(protected)/team-member/_page.tsx"
+import InitProfilePage from "./(driver)/(protected)/init-profile/_page.tsx"
 
 export {
    RootLayout,
 
    IntroductionPage,
+   InitProfilePage,
    DriverLayout,
    ProtectedDriverLayout,
-   DashboardPage,
+   HomePage,
 
    SystemLayout,
    ProtectedSystemLayout,
