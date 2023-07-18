26b9b8e887db686b4b5db386f7cd32ce40a891f7
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-18 14:22:13 +0700

    Update _layout.tsx

--- src/pages/(driver)/_layout.tsx
+++ src/pages/(driver)/_layout.tsx
@@ -1,22 +1,30 @@
 import {Auth0Provider} from "@auth0/auth0-react"
+import {Box} from "@mui/material"
 import {Outlet} from "react-router-dom"
+import Header from "./header.tsx"
 
 function DriverLayout()
 {
    return (
       <Auth0Provider {...cfn.authProvider}>
-         <Outlet/>
+         <Header/>
+         <Box {...cfn.outletContainer}><Outlet/></Box>
       </Auth0Provider>
    )
 }
 
 const cfn = {
+
    authProvider: {
       domain: "click4wash-dev.jp.auth0.com",
       clientId: "3efV1oq9w0pwK3WyeISPuId8eEfOXPzb",
       authorizationParams: {
          redirect_uri: window.location.origin
       }
+   },
+
+   outletContainer: {
+      marginTop: 9
    }
 }
 
