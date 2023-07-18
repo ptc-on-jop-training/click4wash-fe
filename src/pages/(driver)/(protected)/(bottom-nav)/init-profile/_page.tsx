3be678b28d027b3e8342575694ab011ff8552b9f
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-18 14:22:24 +0700

    Create _page.tsx

---
+++ src/pages/(driver)/(protected)/(bottom-nav)/init-profile/_page.tsx
@@ -0,0 +1,12 @@
+import {Box, Typography} from "@mui/material"
+
+function InitProfilePage()
+{
+   return (
+      <Box>
+         <Typography>can't believe this shit is real, this is init profile page</Typography>
+      </Box>
+   )
+}
+
+export default InitProfilePage
