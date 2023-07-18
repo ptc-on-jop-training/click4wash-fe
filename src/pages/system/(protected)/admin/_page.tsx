ba415ca218f566aebd3ea0711bb1cd22589b1ab7
nguyet.ho <nguyet.ho23@student.passerellesnumeriques.org>
2023-07-18 13:36:18 +0700

    refactor `page` for admin

--- src/pages/system/(protected)/admin/_page.tsx
+++ src/pages/system/(protected)/admin/_page.tsx
@@ -1,12 +1,66 @@
 import DashboardLayout from "./_layout.tsx"
 import {TabNav, TabRouteType} from "../../../../components"
 import {useMemo} from "react"
-import {NotificationsSharp, RequestPageSharp} from "@mui/icons-material"
+import {NotificationsSharp, RequestPageSharp, Language} from "@mui/icons-material"
 import UsersTable from "./users-table.tsx"
 import {Box, Typography} from "@mui/material"
 import PackingSlotTable from "./packing-slot/packing-slot-table.tsx"
 import AddressTable from "./address/address-table.tsx"
+import BookingTable from "./list-booking/booking-table.tsx"
+
 function AdminDashboardPage() {
+   const data = [
+      {
+         id: "1",
+         date: new Date(),
+         chargeBattery: true,
+         location: {
+            address: "123 ABC Street",
+            parkingSlot: "A1",
+            timeslot: 1,
+         },
+         driver: {
+            fullName: "John Doe",
+            phoneNumber: "123456789",
+            email: "johndoe@example.com",
+            vehicleInfo: "Car Model XYZ",
+         },
+         teamMember: {
+            fullName: "Jane Smith",
+            phoneNumber: "987654321",
+            email: "janesmith@example.com",
+         },
+         status: "requested",
+         comment: "Great service!",
+         rating: 5,
+         feedback: "Very satisfied with the washing service.",
+      },
+      {
+         id: "2",
+         date: new Date(),
+         chargeBattery: false,
+         location: {
+            address: "456 XYZ Street",
+            parkingSlot: "B2",
+            timeslot: 2,
+         },
+         driver: {
+            fullName: "Alice Johnson",
+            phoneNumber: "987654321",
+            email: "alicejohnson@example.com",
+            vehicleInfo: "Car Model ABC",
+         },
+         teamMember: {
+            fullName: "Bob Williams",
+            phoneNumber: "123456789",
+            email: "bobwilliams@example.com",
+         },
+         status: "done",
+         comment: "Great service!",
+         rating: 5,
+         feedback: "Very satisfied with the washing service.",
+      },
+   ]
    const routes = useMemo<TabRouteType[]>(() => {
       return [
          {
@@ -28,7 +82,7 @@ function AdminDashboardPage() {
                <RequestPageSharp fontSize={"small"}/>
                <Typography>List Booking</Typography>
             </Box>,
-            element: <UsersTable/>
+            element: <BookingTable bookingList={data}/>
          },
          {
             label: <Box {...styles.tab}>
@@ -64,7 +118,7 @@ function AdminDashboardPage() {
    return (
       <DashboardLayout
          main={<Box {...styles.mainLayout}>
-            <TabNav routes={routes}/>
+            <TabNav routes={routes} rightSlot={<Language fontSize={"large"} color={"info"}/>}/>
          </Box>}
       />
    )
