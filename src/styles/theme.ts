47f163c205312ae3fb114996b2cf193d2781d57f
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-21 11:02:57 +0700

    Update theme.ts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,28 @@
-import {createTheme} from "@mui/material";
+import {createTheme} from "@mui/material"
 
 const Theme = createTheme({
+   palette: {
+      text: {
+         primary: "#374259"
+      },
+      primary: {
+         main: "#5C8984"
+      },
+   },
    components: {
       MuiContainer: {
          defaultProps: {
             maxWidth: false
          }
       },
+
+      MuiButton: {
+         styleOverrides: {
+            root: {
+               boxShadow: "none"
+            }
+         }
+      }
    }
 })
 
