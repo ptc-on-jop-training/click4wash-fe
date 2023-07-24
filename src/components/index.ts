d0ae88e119cc564f48300afb2f64c79ea2093c5f
nguyet.ho <nguyet.ho23@student.passerellesnumeriques.org>
2023-07-25 01:24:35 +0700

    update code

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,12 +4,13 @@ import BookingPreview from "./surfaces/booking-preview.tsx"
 import TabLabel from "./layer/tab-label.tsx"
 import UserOptionMenu from "./data-display/user-option-menu.tsx"
 import CreateVehicleProfileFormModal from "./inputs/create-vehicle-profile-form-modal.tsx"
-
+import TableToolbar from "./data-display/table-toolbar.tsx"
 export {
    PageLoading,
    SectionTitle,
    BookingPreview,
    TabLabel,
    UserOptionMenu,
-   CreateVehicleProfileFormModal
+   CreateVehicleProfileFormModal,
+   TableToolbar
 }
