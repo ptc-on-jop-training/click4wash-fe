37c14c48c564f05534c11e8adf43eb150134c900
ĐXV <88564890+Xuan-Vinh-19-04-2002abcd@users.noreply.github.com>
2023-07-25 21:52:46 +0700

    fix add provider in page layout

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -3,17 +3,21 @@ import {CssBaseline, ThemeProvider} from "@mui/material"
 import {Outlet} from "react-router-dom"
 import {Auth0Config} from "../services/auth0"
 import {Theme} from "../styles"
+import {Provider} from "react-redux"
+import {RootStore} from "../stores"
 
 function RootLayout()
 {
    return (
-      <Auth0Provider {...Auth0Config}>
-         <ThemeProvider theme={Theme}>
-            <CssBaseline>
-               <Outlet/>
-            </CssBaseline>
-         </ThemeProvider>
-      </Auth0Provider>
+      <Provider store={RootStore}>
+         <Auth0Provider {...Auth0Config}>
+            <ThemeProvider theme={Theme}>
+               <CssBaseline>
+                  <Outlet/>
+               </CssBaseline>
+            </ThemeProvider>
+         </Auth0Provider>
+      </Provider>
    )
 }
 
