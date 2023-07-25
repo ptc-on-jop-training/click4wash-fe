e582164fa6a8a77380c6378c40e5af1ea1c47128
vinh.dang <vinh.dang23@student.passerellesnumeriques.org>
2023-07-25 10:38:23 +0700

    fix css in the _page.tsx

--- src/pages/(protected)/admin/list-account/_page.tsx
+++ src/pages/(protected)/admin/list-account/_page.tsx
@@ -1,20 +1,19 @@
-import {Box} from "@mui/material"
 import {accountsData} from "./list-data-account/list-data-account.tsx"
 import AccountTable from "./account-table.tsx"
 
 function AccountPage() {
    return (
-      <Box>
-         <AccountTable accountList={accountsData} {...cfn.accountTable}/>
-      </Box>
+      <AccountTable accountList={accountsData} {...cfn.accountTable}/>
    )
 }
 
 const cfn = {
    accountTable:{
       sx:{
-         height: 772
+         height: "100%",
+         maxHeight: "calc(100vh - 129px)",
       }
    }
 }
+
 export default AccountPage
