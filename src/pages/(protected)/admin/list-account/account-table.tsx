99c403d7a6031703e8914a7d28990c8d153bac1f
vinh.dang <vinh.dang23@student.passerellesnumeriques.org>
2023-07-25 10:41:00 +0700

    fix import AccountResponse

--- src/pages/(protected)/admin/list-account/account-table.tsx
+++ src/pages/(protected)/admin/list-account/account-table.tsx
@@ -1,60 +1,67 @@
-import {DataGrid, GridRenderCellParams, GridColDef,GridToolbar} from '@mui/x-data-grid'
+import {DataGrid, GridColDef, GridRenderCellParams, GridToolbar} from '@mui/x-data-grid'
 import {useEffect, useState} from "react"
-import RoleChip from "../../../../components/data-display/role-chip.tsx"
-import AccountResponse from "../../../../services/auth0/dtos/account-response.ts"
+import {RoleChip} from "../../../../components"
+import {AccountResponse} from "../../../../services/auth0"
 import AccountStatus from "./account-status.tsx"
-import {SxProps} from "@mui/material"
-import {merge} from "lodash"
+import {Box, SxProps} from "@mui/material"
 
 interface AccountTableProps {
-    accountList: AccountResponse[],
-    sx?: SxProps
+   accountList: AccountResponse[],
+   sx?: SxProps
 }
-
-
 
 function AccountTable(props: AccountTableProps) {
    const [rows, setRows] = useState<any[]>([])
-   useEffect(() =>{
-      setRows( props.accountList?.map((account)=> {
-         return{
+
+   useEffect(() => {
+      setRows(props.accountList?.map((account) => {
+         return {
             id: account.id,
             fullName: account.username,
             email: account.email,
             role: account.role,
             status: account.status
          }
-      }))},[props.accountList]
-   )
+      }))
+   }, [props.accountList])
+
    return (
-      <DataGrid
-         {...merge(cfg.sx,{sx: props.sx})}
-         columns={columns}
-         rows={rows}
-         slots={{
-            toolbar: GridToolbar
-         }}
-      />
+      <Box sx={props.sx}>
+         <DataGrid
+            {...cfg.table}
+            columns={columns}
+            rows={rows}
+            slots={{toolbar: GridToolbar}}
+         />
+      </Box>
    )
 }
 
-
 const cfg = {
-   sx: {
-      border: "none"
+   container: {
+
+   },
+   table: {
+      sx: {
+         border: "none",
+         height: "100%"
+      }
    }
 }
 
-
-const columns: GridColDef[] = [
-   {field: 'id', headerName: 'Id', flex: 0.3, headerClassName: 'bold-header', align: "center", headerAlign: "center"},
-   {field: 'fullName', headerName: 'Full Name', flex: 0.5},
-   {field: 'email', headerName: 'Email', flex: 0.5},
-   {field: 'role', headerName: 'Role', flex: 0.5, renderCell: (params: GridRenderCellParams) => <RoleChip role={params.value}/>},
-   {field: 'status', headerName: 'Status', flex: 0.5, renderCell: (params: GridRenderCellParams) => <AccountStatus status={params.value}/>}
-]
-
-
+const columns: GridColDef[] = [{
+   field: 'id', headerName: 'Id', flex: 0.3, headerClassName: 'bold-header', align: "center", headerAlign: "center"
+}, {field: 'fullName', headerName: 'Full Name', flex: 0.5}, {field: 'email', headerName: 'Email', flex: 0.5}, {
+   field: 'role',
+   headerName: 'Role',
+   flex: 0.5,
+   renderCell: (params: GridRenderCellParams) => <RoleChip role={params.value}/>
+}, {
+   field: 'status',
+   headerName: 'Status',
+   flex: 0.5,
+   renderCell: (params: GridRenderCellParams) => <AccountStatus status={params.value}/>
+}]
 
 
 export default AccountTable
