40a3ed2b778d4dee16dd5c3d06ff564c883e36c0
vinh.dang <vinh.dang23@student.passerellesnumeriques.org>
2023-07-25 13:54:46 +0700

    fix format code

--- src/components/data-display/role-chip.tsx
+++ src/components/data-display/role-chip.tsx
@@ -1,16 +1,20 @@
 import {Chip} from "@mui/material"
 import {useEffect, useState} from "react"
-import Role from "../../services/auth0/consts/role.ts"
+import {Role} from "../../services/auth0"
 
 interface RoleChipProps {
-    role: string
-    size?: "small" | "medium"
+   role: string
+   size?: "small" | "medium"
 }
 
 function RoleChip(props: RoleChipProps) {
    const [state, setState] = useState<{ color: any, label: string }>()
 
    useEffect(() => {
+      handleRole()
+   }, [props.role])
+
+   const handleRole = () => {
       switch (props.role) {
       case Role.admin:
          setState({color: "secondary", label: "admin"})
@@ -22,9 +26,10 @@ function RoleChip(props: RoleChipProps) {
          setState({color: "success", label: "team-member"})
          break
       }
-   }, [props.role])
+   }
 
-   return <Chip  size={props.size ?? "medium"} variant={"filled"} color={state?.color} label={state?.label}/>
+   return <Chip size={props.size ?? "medium"} variant={"filled"} color={state?.color} label={state?.label}/>
 }
+
 
 export default RoleChip
