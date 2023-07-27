08b3754af35cc83aa3aedb11d672afbd8b122388
traitimtrongvang <139474932+traitimtrongvang@users.noreply.github.com>
2023-07-27 07:38:05 +0700

    Revert "feat create account-team-member-request"

--- src/services/auth0/index.ts
+++ src/services/auth0/index.ts
@@ -3,13 +3,11 @@ import Auth0Config from "./consts/auth0-config.ts"
 import AccountStatusEnum  from "./consts/account-status.ts"
 import AccountResponse from "./dtos/account-response.ts"
 import GetAccountList from "./usecases/get-account-list.ts"
-import AccountTeamMemberRequest from "./dtos/account-team-member-request.ts"
 
 export {
    Role,
    Auth0Config,
    AccountStatusEnum,
    type AccountResponse,
-   GetAccountList,
-   type AccountTeamMemberRequest
+   GetAccountList
 }
