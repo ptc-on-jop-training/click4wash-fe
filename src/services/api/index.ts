22d03c264facff291ca1254b05e02916860c1351
ĐXV <88564890+Xuan-Vinh-19-04-2002abcd@users.noreply.github.com>
2023-08-04 13:29:40 +0700

    fix delete wash status

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -14,7 +14,6 @@ import VehicleResponse from "./dtos/vehicle-response.ts"
 import GetVehicleList from "./usecases/get-vehicle-list.ts"
 import CreateVehicleRequest from "./dtos/create-vehicle-request.ts"
 import CreateVehicle from "./usecases/create-vehicle.ts"
-import WashStatus from "./consts/wash-status.ts"
 import BookingResponse from "./dtos/booking-response.ts"
 
 export {
@@ -39,5 +38,4 @@ export {
    BookingStatus,
    TimeSlots,
    VehicleType,
-   WashStatus
 }
