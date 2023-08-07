0762deaa8e14540574162af2ad680ab77285bf7a
ĐXV <88564890+Xuan-Vinh-19-04-2002abcd@users.noreply.github.com>
2023-08-07 14:16:28 +0700

    feat create booking status

---
+++ src/pages/(protected)/admin/list-booking/booking-status.tsx
@@ -0,0 +1,37 @@
+import {Chip} from "@mui/material"
+import {useEffect, useState} from "react"
+import {BookingStatusEnum} from "../../../../services/api"
+
+interface BookingStatusProps {
+   status: BookingStatusEnum
+   size?: "small" | "medium"
+}
+
+
+function BookingStatus(props: BookingStatusProps) {
+   const [state, setState] = useState<{ color: any, label: string }>()
+   useEffect(() => {
+      switch (props.status) {
+      case BookingStatusEnum.processing:
+         setState({color: "error", label: "processing"})
+         break
+      case BookingStatusEnum.accepted:
+         setState({color: "secondary", label: "accepted"})
+         break
+      case BookingStatusEnum.refused:
+         setState({color: "default", label: "refused"})
+         break
+      case BookingStatusEnum.done:
+         setState({color: "success", label: "done"})
+         break
+      }
+   }, [props.status])
+
+
+   return <Chip color={state?.color} size={props.size ?? "medium"} variant={"filled"} label={state?.label}/>
+}
+
+
+
+
+export default BookingStatus
