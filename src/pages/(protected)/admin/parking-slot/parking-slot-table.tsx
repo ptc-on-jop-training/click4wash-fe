82d696be7251af8884660871797d874902ea2310
ĐXV <88564890+Xuan-Vinh-19-04-2002abcd@users.noreply.github.com>
2023-08-16 16:17:43 +0700

    feat translate parking slot

--- src/pages/(protected)/admin/parking-slot/parking-slot-table.tsx
+++ src/pages/(protected)/admin/parking-slot/parking-slot-table.tsx
@@ -4,6 +4,7 @@ import {useSelector} from "react-redux"
 import {RootStateType} from "../../../../stores"
 import ParkingSlotToolBar from "./parking-slot-toolbar.tsx"
 import ParkingSlotStatus from "./parking-slot-status.tsx"
+import {useTranslation} from "react-i18next"
 
 interface PackingSlotTableProps {
    sx?: SxProps
@@ -13,6 +14,23 @@ function ParkingSlotTable(props: PackingSlotTableProps) {
 
    const parkingSlotList = useSelector((state: RootStateType) => state.parkingSlot.parkingSlotList)
 
+   const [t] = useTranslation('trans')
+
+   const columns: GridColDef[] = [
+      {field: 'id', headerName: t("admin.ParkingSlot.id"), flex: 0.2, align: "center", headerAlign: "center"},
+      {field: 'locationName', headerName: t("admin.ParkingSlot.location"), flex: 0.2},
+      {field: 'name', headerName:  t("admin.ParkingSlot.parkingSlot"), flex: 0.2,},
+      {field: 'address', headerName: t("admin.ParkingSlot.address"), flex: 0.4,
+         renderCell: (params: GridRenderCellParams) => {
+            const addressValue = params.row.address || {}
+            return Object.values(addressValue).join(', ')}
+      },
+      {field: 'status', headerName: t("admin.ParkingSlot.Status.status"), flex: 0.2,
+         renderCell: (params: GridRenderCellParams) => (
+            <ParkingSlotStatus isBusy={params.row.isBusy}/>
+         ),
+      },
+   ]
    return (
       <DataGrid
          sx={props.sx}
@@ -24,20 +42,6 @@ function ParkingSlotTable(props: PackingSlotTableProps) {
    )
 }
 
-const columns: GridColDef[] = [
-   {field: 'id', headerName: 'Id', flex: 0.2, align: "center", headerAlign: "center"},
-   {field: 'locationName', headerName: 'Location', flex: 0.2},
-   {field: 'name', headerName: 'Parking Slot', flex: 0.2,},
-   {field: 'address', headerName: 'Address', flex: 0.4,
-      renderCell: (params: GridRenderCellParams) => {
-         const addressValue = params.row.address || {}
-         return Object.values(addressValue).join(', ')}
-   },
-   {field: 'status', headerName: 'Status', flex: 0.2,
-      renderCell: (params: GridRenderCellParams) => (
-         <ParkingSlotStatus isBusy={params.row.isBusy}/>
-      ),
-   },
-]
+
 
 export default ParkingSlotTable
