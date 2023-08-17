2f5518df8dc2a3f87413f477667da7c22b1565ef
TaiLinhPN <linh.nguyenbuitai23@student.passerellesnumeriques.org>
2023-08-17 14:56:31 +0700

    fix: time slot

--- src/services/api/consts/time-slots.ts
+++ src/services/api/consts/time-slots.ts
@@ -2,51 +2,45 @@ import TimeSlot from "../entities/time-slot.ts"
 
 const TimeSlots = Object.freeze({
    A: <TimeSlot>{
-      start: new Date("3000-01-01T06:00:00"),
-      end: new Date("3000-01-01T07:00:00"),
+      start: "06:00",
+      end: "07:00",
    },
    B: <TimeSlot>{
-      start: new Date("3000-01-01T08:00:00"),
-      end: new Date("3000-01-01T09:00:00"),
+      start: "08:00",
+      end: "08:00",
    },
    C: <TimeSlot>{
-      start: new Date("3000-01-01T09:00:00"),
-      end: new Date("3000-01-01T10:00:00"),
+      start: "09:00",
+      end: "10:00",
    },
    D: <TimeSlot>{
-      start: new Date("3000-01-01T11:00:00"),
-      end: new Date("3000-01-01T12:00:00"),
+      start: "11:00",
+      end: "12:00",
    },
    E: <TimeSlot>{
-      start: new Date("3000-01-01T13:00:00"),
-      end: new Date("3000-01-01T14:00:00"),
+      start: "13:00",
+      end: "14:00",
    },
    F: <TimeSlot>{
-      start: new Date("3000-01-01T15:00:00"),
-      end: new Date("3000-01-01T16:00:00"),
+      start: "15:00",
+      end: "16:00",
    },
    G: <TimeSlot>{
-      start: new Date("3000-01-01T17:00:00"),
-      end: new Date("3000-01-01T18:00:00"),
+      start: "17:00",
+      end: "18:00",
    },
    H: <TimeSlot>{
-      start: new Date("3000-01-01T19:00:00"),
-      end: new Date("3000-01-01T20:00:00"),
+      start: "19:00",
+      end: "20:00",
    },
    I: <TimeSlot>{
-      start: new Date("3000-01-01T21:00:00"),
-      end: new Date("3000-01-01T22:00:00"),
+      start: "21:00",
+      end: "22:00",
    },
 })
 function FormatTimeSlot(timeSlot: TimeSlot) {
-
    const { start, end } = timeSlot
-   const startDate = new Date(start)
-   const endDate = new Date(end)
-   const startTime = `${startDate.getHours()}:${startDate.getMinutes()}`
-   const endTime = `${endDate.getHours()}:${endDate.getMinutes()}`
-
-   return `${startTime} - ${endTime}`
+   return `${start} -> ${end}`
 }
 
 export {FormatTimeSlot}
