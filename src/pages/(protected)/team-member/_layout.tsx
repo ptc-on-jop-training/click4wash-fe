65db4fb416b2ac774c349d82d531b5707a328491
TaiLinhPN <linh.nguyenbuitai23@student.passerellesnumeriques.org>
2023-08-17 18:28:39 +0700

    feat: update responsive

--- src/pages/(protected)/team-member/_layout.tsx
+++ src/pages/(protected)/team-member/_layout.tsx
@@ -1,38 +1,40 @@
-import {Box, Container} from "@mui/material"
-import {Outlet} from "react-router-dom"
+import { Box, Container } from "@mui/material"
+import { Outlet } from "react-router-dom"
 import Header from "./header.tsx"
 import BottomNav from "./bottom-nav.tsx"
 
-function TeamMemberAppLayout()
-{
+function TeamMemberAppLayout() {
    return (
       <Box>
-         <Header {...cfn.header}/>
+         <Header {...cfn.header} />
          <Container {...cfn.outletCont}>
-            <Outlet/>
+            <Outlet />
          </Container>
-         <BottomNav {...cfn.bottomNav}/>
+         <BottomNav {...cfn.bottomNav} />
       </Box>
    )
 }
 
 const cfn = {
    header: {
-      position: "sticky" as const
+      position: "sticky" as const,
    },
 
    outletCont: {
       sx: {
+         maxWidth : "sm",
          paddingTop: 1.5,
-      }
+      },
    },
 
    bottomNav: {
       sx: {
          position: "fixed",
-         bottom: 0, left: 0, right: 0
-      }
-   }
+         bottom: 0,
+         left: 0,
+         right: 0,
+      },
+   },
 }
 
 export default TeamMemberAppLayout
